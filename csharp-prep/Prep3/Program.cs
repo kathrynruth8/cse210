c8c33592c98321c3fb7f5f2a1c4dbf03267250bc
Kathryn Lambert <lam20005@byui.edu>
2023-09-20 22:15:12 -0600

    commit for cse 210 prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        // // Start by asking the user for the magic number
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
+
+        // Ask the user for a guess.
+        // Console.Write("What is your guess? ");
+        // int guess = int.Parse(Console.ReadLine());
+    
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            // Using an if statement, determine if the user needs to guess higher or lower next time, or tell them if they guessed it.
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        }
 }
