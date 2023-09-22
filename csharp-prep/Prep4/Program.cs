d812d38c04830d9b9c7224409e508d0eaf1fb333
Kathryn Lambert <lam20005@byui.edu>
2023-09-21 22:21:26 -0600

    commit prep 4&5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        // Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // PART 1 - Compute the sum, or total, of the numbers in the list.
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Compute the average of the numbers in the list.
+        float average = ((float)sum / numbers.Count);
+        Console.WriteLine($"The average is: {average}");
+
+        // Find the maximum, or largest, number in the list.
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
